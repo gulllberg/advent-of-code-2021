3447b461736c7d79ce89572ebb0fa594f9caea43
Daniel Gullberg <daniel@gullberg.dev>
2021-12-05 12:25:55 +0100

    Day 5

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/math.combinatorics "0.1.6"]
                  [ysera "2.0.2" :exclusions [com.google.errorprone/error_prone_annotations com.google.code.findbugs/jsr305]]]
   :repl-options {:init-ns advent-of-code-2021.core})
